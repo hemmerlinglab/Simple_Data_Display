6bb97b253f600c243ee88b4014ba5433c3351749
Hemmerling <hemmerlinglab@gmail.com>
2019-04-09 17:03:10 -0700

    adding more

--- show_data.py
+++ show_data.py
@@ -8,25 +8,29 @@ import csv
 import datetime
 
 
-
-main_path = '/home/lab-42/data_folder/K_Tests/'
+main_path = '/home/molecules/skynet/Data/K_Tests/'
+#main_path = '/home/lab-42/data_folder/K_Tests/'
 
 my_today = datetime.datetime.today().strftime('%Y-%m-%d')
 
-my_time = '14-53-13'
+my_time = '16-28-15'
 
 f1 = main_path + my_today + '-' + my_time + '_1.csv'
 f2 = main_path + my_today + '-' + my_time + '_2.csv'
+fs = main_path + my_today + '-' + my_time + '_set.csv'
 
 
 d1f = open(f1,'r')
 d2f = open(f2,'r')
+dsf = open(fs,'r')
 
 d1r = csv.reader(d1f)
 d2r = csv.reader(d2f)
+dsr = csv.reader(dsf)
 
 d1 = np.array([])
 d2 = np.array([])
+ds = np.array([])
 for row in d1r:
 
     hlp_row = ",".join(row).split(',')[:-1]
@@ -43,6 +47,8 @@ for row in d2r:
     if len(hlp)>0:
         d2 = np.vstack((d2, hlp)) if d2.size else hlp
 
+for row in dsr:
+	print(row)
 
 #d1 = np.array(d1, dtype = np.float)
 
@@ -67,3 +73,4 @@ plt.show()
 
 d1f.close()
 d2f.close()
+dsf.close()
