3a6a0aeed33728ba067c0af6d70bab472438fbff
Hemmerling <hemmerlinglab@gmail.com>
2019-06-13 17:28:28 -0700

    removing unnessary files

--- show_data.py
+++ show_data.py
@@ -1,7 +1,19 @@
 """ Original version which saves the csv files
 Uses the calculation of finding the frequency spacing between the lines
 New modifications: fits the data, and finds params such as temperature, wavemeter offset. Numbers might be off and still needs to be double checked """
+"""This version is the current working code, it fits two lines of potassium, the updated fit to include the multiple lines are in the code labeled John, but the fitting is quite off currently """
 
+
+""" Plan: Plot the fit with the natural line width of the lines"""
+
+""" Current problems:
+    color plot no longer works
+    
+    Questions: 
+       1. How does this line work?
+       mod_x and mod_y is not passed in, this is the first time they are mentioned
+       (mod_x,mod_y) = fcn2min(result.params, fit_x, [], return_plot = True)
+       2. What unit is the time in? not sure how that works """
 import numpy as np
 import matplotlib
 
@@ -19,9 +31,9 @@ from fit_spectrum import *
 #main_path = '/home/molecules/skynet/Data/K_Tests/'
 #main_path = '/home/lab-42/data_folder/K_Tests/'
 #main_path = '/home/lab-42/software/github/Prehistoric-Data-Acquisition/'
-#main_path = '/home/molecules/software/Prehistoric-Data-Acquisition/'
+main_path = '/home/molecules/software/Prehistoric-Data-Acquisition/'
 #main_path = '/Users/johnr/Documents/Github/Prehistoric-Data-Acquisition/'
-main_path = '/Users/kaylajane/software/Prehistoric-Data-Acquisition/'
+#main_path = '/Users/kaylajane/software/Prehistoric-Data-Acquisition/'
 
 my_today = datetime.datetime.today().strftime('%Y-%m-%d')
 
@@ -63,67 +75,45 @@ for row in d1r:
     if len(hlp)>0:
         d1 = np.vstack((d1, hlp)) if d1.size else hlp
 
-#for row in d2r:
-#
-#    hlp_row = ",".join(row).split(',')[:-1]
-#    hlp = np.array(hlp_row, dtype = np.float)
-#
-#    if len(hlp)>0:
-#        print(len(hlp))
-#        d2 = np.vstack((d2, hlp)) if d2.size else hlp
-
 for row in dsr:
 
 	setpoint = ','.join(row).split(',')[0]
 	if setpoint != '':
 		ds = np.append(ds,float(setpoint))
 
-line_act = 391.01617
-ds = ds - line_act
+# ds is the frequency in THz of the wavemeter at the time of any given data point
 
-ds = ds * 1e12/1e6
+line_act = 391.01617 #actual frequency of K39 D2 line
+ds = ds - line_act # subtract by the actual line
 
-d1 = np.array(d1, dtype = np.float)
+ds = ds * 1e12/1e6 # conversion to MHz
 
-plt.figure()
-plt.plot(d1)
-plt.plot(d2)
-ch2_scale = 2
-d2 = d2*ch2_scale
-#
-ch1_scale = .05
-d1 = d1*ch1_scale
-dt = np.linspace(0, 2.5, d1.shape[1]) * 10.0
-#
-plt.figure()
-#
-plt.plot(dt, d1[25, :])
-#
-## figure 2 spectrum stuff
-plt.figure()
-#
-
+# each slice is delta time units wide, avergaing over the delta time units for each point
 def get_time_slot(arr, minx, delta = 20):
 
     return -np.mean(arr[:, minx:(minx+delta)], axis = 1)
 
-
+#initialization
 res_t = []
 res_T = []
 res_shift = []
 
-# fitting
 
-for k in range(10,100):
+# Time to range over
+start_t = 10
+end_t   = 100
+
+for k in range(start_t,end_t):
 
     print(k)
     fit_x = ds * 1e6 + line_act * 1e12
     
-    my_t = 100 + k * 10
+    my_t = end_t + k * start_t
     fit_y = get_time_slot(d1, my_t)
 
     result = my_fit(fit_x, fit_y)
-
+    
+    # uses mod_x and mod_y parameters for function to minimize (comes from fit_spectrum)
     (mod_x, mod_y) = fcn2min(result.params, fit_x, [], return_plot = True)
 
     res_t.append(my_t)
@@ -135,33 +125,29 @@ for k in range(10,100):
 
     if k == 60:
         plt.figure()
-        plt.plot( (fit_x - line_act*1e12)/1e6, fit_y, 'ko')
-        plt.plot( (mod_x - line_act*1e12)/1e6, mod_y, 'r-')
+        plt.plot( (fit_x - line_act*1e12)/1e6, fit_y, 'ko',label='data')
+        plt.plot( (mod_x - line_act*1e12)/1e6, mod_y, 'r-',label='fitting')
         plt.xlabel('Frequency MHz',fontsize=16)
-        plt.ylabel('Strength of the line (arb)',fontsize=16)
+        plt.ylabel('Strength of line (abs)',fontsize=16)
+        plt.tick_params(labelsize=16) #tick size
+        plt.tick_params(direction='in') #tick direction
+        plt.legend(fontsize=14)
+plt.figure()
+plt.plot( res_t, res_T, 'mo-') # o is the circle marker
+plt.xlabel('Time',fontsize=16)
+plt.ylabel('Temperature (K)',fontsize=16)
+plt.tick_params(labelsize=16) #tick size
+plt.tick_params(direction='in') #tick direction
 
 plt.figure()
-plt.plot( res_t, res_T, 'o-')
-
-plt.figure()
-plt.plot( res_t, res_shift, 'o-')
-
-
-
+plt.plot( res_t, res_shift, 'mo-')
+plt.xlabel('Time',fontsize=16)
+plt.ylabel('Temperature (K)',fontsize=16)
+plt.tick_params(labelsize=16) #tick size
+plt.tick_params(direction='in') #tick direction
 plt.show()
 
-
-# based on level structure from the paper
-non_hyper39 = 766.701
-pf3sf2_39 = 14.4-173.1
-pf2sf1_39 = 288.6-6.7
-
-#non_hyper41 = 
-freq_diff = abs(pf3sf2_39-pf2sf1_39)
-print('Frequency Difference MHz: {}'.format(freq_diff))
-
-
-plt.xlabel('Frequency Difference (MHz) from from {} GHs'.format(line_act))
+#plt.xlabel('Frequency Difference (MHz) from from {} GHs'.format(line_act))
 
 plt.figure()
 
